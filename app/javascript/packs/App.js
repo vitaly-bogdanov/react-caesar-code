b42299657684fc596b8deb099fde3aa6b69d0a58
Vitaly Bogdanov <rootkaliboot@gmail.com>
2020-02-17 01:43:59 +0900

    правки

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import AppRoutes from './config/AppRoutes';
 import { checkLoggedInThunk } from './redux/middlewares';
 import { connect } from 'react-redux';
+import 'bulma/bulma';
+import './app.scss';
 
 const App = props => {
 
